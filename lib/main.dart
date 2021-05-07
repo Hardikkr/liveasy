00f4d0bd185f5492208311057ac9f766e5fed480
Hardikkr <hardik.kumar18feb@gmail.com>
2021-05-08 03:09:27 +0530

    Mobile Number page created

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liveasy',
+      home: MobileNumber001(),
+    );
+  }
+}
+
+class MobileNumber001 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final body = Container(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(Icons.close),
+            SizedBox(height: 50),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Please enter your mobile number",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "You'll receive a 4 digit code\nto verify next.", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, color: Colors.black54),
+                ),
+                SizedBox(height: 35),
+                Container(
+                  height: 50,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(0))
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                        width: 30,
+                        child: Image.asset("assets/indian-flag.png"),
+                      ),
+                      Text(
+                        "    +91    -    ",
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: "Mobile Number",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 35),
+                Container(
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff2E3B62),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(0))
+                      )
+                    ),
+                    child: Text(
+                      "CONTINUE",
+                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
