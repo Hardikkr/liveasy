b609dea4bf497f308dfc9b3090494cd4ce04bcce
Hardikkr <hardik.kumar18feb@gmail.com>
2021-05-08 20:02:27 +0530

    Profile Selection page created

---
+++ lib/screens/profile_selection.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+class ProfileSelection extends StatefulWidget {
+  @override
+  _ProfileSelectionState createState() => _ProfileSelectionState();
+}
+
+class _ProfileSelectionState extends State<ProfileSelection> {
+  @override
+  Widget build(BuildContext context) {
+    bool selectedShipper = false;
+    bool selectedTransporter = false;
+
+    void makeShipperProfile() {
+      setState(() {
+        selectedTransporter = false;
+        selectedShipper = true;
+      });
+    }
+
+    void makeTransporterProfile() {
+      setState(() {
+        selectedShipper = false;
+        selectedTransporter = true;
+      });
+    }
+
+    final body = SafeArea(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 75),
+            Text(
+              "Please select your profile",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            SizedBox(height: 35),
+            InkWell(
+              onTap: () {
+                makeShipperProfile();
+              },
+              child: Container(
+                height: 100,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(0),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    selectedShipper
+                        ? Icon(Icons.radio_button_checked)
+                        : Icon(Icons.radio_button_off),
+                    SizedBox(width: 10),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: Image.asset("assets/shipper.png"),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Shipper",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 22,
+                            ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet,\nconsectetur adipiscing",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              onTap: () {
+                makeTransporterProfile();
+              },
+              child: Container(
+                height: 100,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(0),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    selectedTransporter
+                        ? Icon(Icons.radio_button_checked)
+                        : Icon(Icons.radio_button_off),
+                    SizedBox(width: 10),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: Image.asset("assets/transporter.png"),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Transporter",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 22,
+                            ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet,\nconsectetur adipiscing",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 35),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xff2E3B62),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(0),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "CONTINUE",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: body,
+    );
+  }
+}
--- lib/screens/verify_phone.dart
+++ lib/screens/verify_phone.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:liveasy/screens/profile_selection.dart';
 
 class VerifyPhone extends StatelessWidget {
   VerifyPhone(this.phoneNumber);
@@ -11,11 +12,11 @@ class VerifyPhone extends StatelessWidget {
     final textBlock = FocusScope.of(context);
 
     Widget textField = Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(5),
       height: 45,
       width: 45,
       color: Color(0xff93D2F3),
-      child: Expanded(
+      child: Center(
         child: TextField(
           onChanged: (str) {
             if (str.length == 1) {
@@ -26,94 +27,105 @@ class VerifyPhone extends StatelessWidget {
           style: TextStyle(fontSize: 25),
           maxLength: 1,
           keyboardType: TextInputType.number,
-          decoration:
-              InputDecoration(counterText: "", border: InputBorder.none),
+          decoration: InputDecoration(
+            counterText: "",
+            border: InputBorder.none,
+          ),
         ),
       ),
     );
 
-    final body = Container(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back_outlined),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(height: 50),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  "Verify Phone",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "Code sent to $phoneNumber",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                ),
-                SizedBox(height: 15),
-                Container(
-                  height: 50,
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  width: double.infinity,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      textField,
-                      textField,
-                      textField,
-                      textField,
-                    ],
+    final body = SafeArea(
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back_outlined),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(height: 30),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Verify Phone",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                   ),
-                ),
-                SizedBox(height: 15),
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    text: "Didn't receive the code? ",
+                  SizedBox(height: 15),
+                  Text(
+                    "Code sent to $phoneNumber",
+                    textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 18, color: Colors.black54),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Request Again",
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                        recognizer: TapGestureRecognizer()..onTap = () {},
-                      )
-                    ],
                   ),
-                ),
-                SizedBox(height: 35),
-                Container(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xff2E3B62),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(0),
+                  SizedBox(height: 15),
+                  Container(
+                    height: 50,
+                    padding: const EdgeInsets.symmetric(horizontal: 80),
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        textField,
+                        textField,
+                        textField,
+                        textField,
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: "Didn't receive the code? ",
+                      style: TextStyle(fontSize: 15, color: Colors.black54),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Request Again",
+                          style: TextStyle(fontSize: 15, color: Colors.black),
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 35),
+                  Container(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfileSelection(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff2E3B62),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(0),
+                          ),
                         ),
                       ),
+                      child: Text(
+                        "VERIFY AND CONTINUE",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 18),
+                      ),
                     ),
-                    child: Text(
-                      "VERIFY AND CONTINUE",
-                      style:
-                          TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
